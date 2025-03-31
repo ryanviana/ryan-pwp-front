20b34dde61945d6f09825364da977a16f5db2f93
Ryan Braz Tintore Viana <ryanviana@usp.br>
2025-03-30 21:03:43 -0300

    fix problem with blog

--- next.config.ts
+++ next.config.ts
@@ -21,6 +21,12 @@ const nextConfig: NextConfig = {
         port: "",
         pathname: "**",
       },
+      {
+        protocol: "https",
+        hostname: "back.pwp.ryanviana.com",
+        port: "",
+        pathname: "**",
+      },
     ],
   },
 };
